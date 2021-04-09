8a0dc4c54b310227451fddeff807038fc4fd3b24
Amakye Oduro Philip <philamakye@gmail.com>
2021-04-09 02:08:30 +0100

    Raw Frontend

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">The Boys Apparel <img src="images/ico1.gif" style="height:25px; border-radius: 30px" class="img-fluid" alt="..."></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col-7">
+            <div class="row mt-5">
+                <div class="col">
+                <div class="card">
+                    <img src="images/image1.jpg" class="card-img-top" style="height: 200px" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">
+                          <center><head>
+                              Alexander Mcqueen
+                          </head></center>
+                        <center><b>$1500</b></center>
+                        <br>
+                         <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="images/image2.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Custom Balmain Mcqueen
+                    </head></center>
+                  <center><b>$1000</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                   </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="images/image3.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Classic SofoPapa 
+                    </head></center>
+                  <center><b>$5000</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                   </p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col">
+                <div class="card">
+                    <img src="images/image4.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Mad Camboo
+                    </head></center>
+                  <center><b>$50000</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                  </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="images/image5.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Women Outing
+                    </head></center>
+                  <center><b>$1000</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                   </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="images/image6.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Devil's Prada
+                    </head></center>
+                  <center><b>$500</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                  </p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col">
+                <div class="card">
+                    <img src="images/image7.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Women's bag
+                    </head></center>
+                  <center><b>$100000</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="images/image8.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Leather bag
+                    </head></center>
+                  <center><b>$15000</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="images/image9.jpg" style="height: 200px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text"><center><head>
+                        Belt
+                    </head></center>
+                  <center><b>$200</b></center>
+                  <br>
+                   <center><button type="button" class="btn btn-outline-warning">Buy</button></center>
+                </p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+           
+          </div>
+          <div class="col-5 mt-5">
+            <div class="col">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label"><b>Email address</b></label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label"><b>Item Description</b></label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  </div>
+                  <div class="row g-2">
+                    <div class="col-md">
+                      <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInputGrid" placeholder="1500" value="1500">
+                        <label for="floatingInputGrid">Total item price</label>
+                      </div>
+                    </div>
+                    <div class="col-md">
+                      <div class="form-floating">
+                        <select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">
+                          <option selected>USD</option>
+                          <option value="1">USD</option>
+                          <option value="2">EURO</option>
+                          <option value="3">CFA</option>
+                        </select>
+                        <label for="floatingSelectGrid">Currency</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row g-2 pt-2">
+                    <div class="col-md">
+                      <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInputGrid" placeholder="1" value="1">
+                        <label for="floatingInputGrid">Shipping Fee</label>
+                      </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-floating">
+                          <input type="email" class="form-control" id="floatingInputGrid" placeholder="0" value="0">
+                          <label for="floatingInputGrid">Tax rate %</label>
+                        </div>
+                      </div>
+                  </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
