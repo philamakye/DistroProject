cd390a3398541edc797d7b4e422dd6d7c3009a22
averagewifiuser <averagewifiuser@gmail.com>
2021-04-17 17:15:37 +0100

    Set-up modification made

---
+++ distroAPI/core/initialize.php
@@ -0,0 +1,19 @@
+<?php
+
+    defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+    defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'xampp'.DS.'htdocs'.DS.'DistroProject'.DS.'distroAPI');
+
+    //includes folder path
+    defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
+    //core folder path
+    defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
+
+
+    //load the config file first
+    require_once(INC_PATH.DS.'config.php');
+
+    //core classes
+//    require_once(CORE_PATH.DS.'post.php');
+//    require_once(CORE_PATH.DS.'category.php');
+
+?>
