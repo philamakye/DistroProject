485eaa5a07eb9b38e027c91f53dc913afd450eab
averagewifiuser <averagewifiuser@gmail.com>
2021-04-17 19:07:51 +0100

    fetch one product fully functional

---
+++ distroAPI/api/read_single.php
@@ -0,0 +1,32 @@
+<?php
+
+    //headers
+    header('Access-Control-Allow-Origin: *'); //allows acces without auth etc
+    header('Content-Type: application/json');
+
+
+    //initializing our API
+    include_once('../core/initialize.php');
+
+    //instantiate the Post class
+
+    $product = new Product($db);
+
+    $product->id = isset($_GET['id']) ? $_GET['id'] : die();
+    $product->read_single();
+
+    $product_arr = array(
+        'id'=>$product->id,
+        'name' => $product->name,
+        'price' => $product->price,
+        'description' => $product->description,
+        'stock' => $product->stock,
+        'category_id' => $product->category_id,
+        'category_name' => $product->category_name
+    );
+
+    //make a json
+    print_r(json_encode($product_arr));
+
+
+?>
--- distroAPI/core/products.php
+++ distroAPI/core/products.php
@@ -47,6 +47,42 @@
         }
         
         
+        //reading a single product
+        public function read_single(){
+            //create query
+            $query = ' SELECT 
+            c.name as category_name,
+            p.id,
+            p.category_id,
+            p.name,
+            p.price,
+            p.description,
+            p.stock,
+            p.created_at 
+            FROM '.$this->table . ' p LEFT JOIN 
+            categories c ON p.category_id = c.id WHERE 
+            p.id=? LIMIT 1';
+            
+            //prepare statement
+            $stmt = $this->conn->prepare($query);
+            
+            //bind params
+            $stmt->bindParam(1, $this->id);
+            
+            //execute query
+            $stmt->execute();
+            
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->name = $row['name'];
+            $this->price = $row['price'];
+            $this->description = $row['description'];
+            $this->stock = $row['stock'];
+            $this->category_id = $row['category_id'];
+            $this->category_name = $row['category_name'];
+        }
+        
+        
         //creating a new product
 //        public function create(){
 //            //create query
